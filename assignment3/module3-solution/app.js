22364b211a384ca60d0c16f01fbca21ecd2d5db6
Burak Karaman <karaman.buraak@gmail.com>
2024-07-25 23:15:50 +0200

    Refactor app.js, index.html, and remove items.html; update directive, controller, and service logic

--- assignment3/module3-solution/app.js
+++ assignment3/module3-solution/app.js
@@ -6,60 +6,45 @@
     .controller("NarrowItDownController", NarrowItDownController)
     .service("MenuSearchService", MenuSearchService)
     .constant("ApiBasePath", "https://coursera-jhu-default-rtdb.firebaseio.com")
-    .directive("foundItems", FoundItems);
+    .directive("foundItems", FoundItemsDirective);
 
-  function FoundItems() {
+  function FoundItemsDirective() {
     var ddo = {
-      templateUrl: "items.html",
+      templateUrl: "foundItems.html",
       scope: {
         items: "<",
         onRemove: "&",
       },
-      controller: FoundItemsDirectiveController,
-      controllerAs: "list",
-      bindToController: true,
     };
-
     return ddo;
-  }
-
-  function FoundItemsDirectiveController() {
-    var list = this;
   }
 
   NarrowItDownController.$inject = ["MenuSearchService"];
   function NarrowItDownController(MenuSearchService) {
-    var menu = this;
-    menu.searchTerm = "";
-    menu.found = [];
-    menu.error = "";
+    var narrowCtrl = this;
+    narrowCtrl.searchTerm = "";
+    narrowCtrl.found = [];
 
-    menu.getMatchedMenuItems = function () {
-      menu.found = [];
-      if (menu.searchTerm) {
-        var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    narrowCtrl.search = function () {
+      narrowCtrl.found = [];
+      if (narrowCtrl.searchTerm.trim() != "") {
+        var promise = MenuSearchService.getMatchedMenuItems(
+          narrowCtrl.searchTerm
+        );
         promise
-          .then(function (response) {
-            menu.found = response;
-            if (menu.found.length === 0) {
-              menu.error = "Nothing found";
-            } else {
-              menu.error = "";
-            }
+          .then(function (result) {
+            narrowCtrl.found = result;
+            console.log(result);
           })
           .catch(function (error) {
-            console.log("Something went terribly wrong.");
+            console.log("Something went wrong: " + error);
+            narrowCtrl.search = "Nothing found 2";
           });
-      } else {
-        menu.error = "Please enter a search term";
       }
     };
 
-    menu.removeItem = function (index) {
-      menu.found.splice(index, 1);
-      if (menu.found.length == 0) {
-        menu.error = "Nothing found";
-      }
+    narrowCtrl.remove = function (index) {
+      narrowCtrl.found.splice(index, 1);
     };
   }
 
@@ -68,25 +53,23 @@
     var service = this;
 
     service.getMatchedMenuItems = function (searchTerm) {
-      return $http({
+      var response = $http({
         method: "GET",
         url: ApiBasePath + "/menu_items.json",
-      }).then(function (result) {
-        // process result and only keep items that match
-        var items = result.data.menu_items;
-        var foundItems = [];
-        searchTerm = searchTerm.toLowerCase();
-        for (var index = 0; index < items.length; index++) {
-          if (
-            items[index].description &&
-            items[index].description.toLowerCase().indexOf(searchTerm) !== -1
-          ) {
-            foundItems.push(items[index]);
-          }
-        }
+      });
 
-        // return processed items
-        return foundItems;
+      return response.then(function (result) {
+        var searchItems = [];
+        var data = result.data;
+
+        for (var category in data) {
+          searchItems.push(
+            data[category].menu_items.filter((item) =>
+              item.description.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+          );
+        }
+        return searchItems.flat();
       });
     };
   }
